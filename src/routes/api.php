ea5a1358d51359c3c56978163a753156b90ea60c
guchio33 <aokihikari3@gmail.com>
2025-10-13 13:00:53 +0900

    add:book一覧取得APIを作成

---
+++ src/routes/api.php
@@ -0,0 +1,8 @@
+<?php
+use App\Http\Controllers\API\BookController;
+use Illuminate\Support\Facades\Route;
+
+// Route::apiResource('books', BookController::class);
+Route::prefix('books')->group(function () {
+  Route::get('/', [BookController::class, 'index']);
+});
